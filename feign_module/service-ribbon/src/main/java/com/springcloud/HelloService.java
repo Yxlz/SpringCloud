38e130c9e41553db46c6fa1870842954e7c28d10
null <616505453@qq.com>
2017-11-16 16:09:01 +0800

    服务消费者（Feign）

---
+++ feign_module/service-ribbon/src/main/java/com/springcloud/HelloService.java
@@ -0,0 +1,19 @@
+package com.springcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Administrator on 2017/11/16 0016.
+ */
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
